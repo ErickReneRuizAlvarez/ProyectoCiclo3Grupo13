3ab84beb5e43f4a60eba1606c43ae42a9d7633ac
elkinruizalv <elferrual@gmail.com>
2022-09-15 00:06:03 -0500

    Commit de elkin 2022-09-15

--- hospg13_be/authProject/urls.py
+++ hospg13_be/authProject/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from authApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('usuario/', views.CrearUsuarioView.as_view()),
+    path('usuario/<int:pk>/', views.UsuarioDetailView.as_view()),
+    path('paciente/', views.CrearPacienteView.as_view()),
+    path('paciente/<int:pk>/', views.PacienteDetailView.as_view()),
+    path('Psalud/', views.CrearPersonalSaludview.as_view()),
+    path('Psalud/<int:pk>/', views.PersonalSaludDetailView.as_view()),
+    path('familiar/', views.CrearFamiliarView.as_view()),
+    path('familiar/<int:pk>/', views.FamiliarDetailView.as_view()),
 ]
