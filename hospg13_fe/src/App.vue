ee9c65f127f4a64e8a21446e6f63ca6dbd39eb5b
ErickReneRuizAlvarez <errerual@gmail.com>
2022-09-24 07:03:34 -0500

    Erick-2022-09-25

---
+++ hospg13_fe/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+
+  export default{
+
+  data: function(){
+  },
+
+  methods:{
+  },
+
+  created: function(){
+  }
+}
+</script>
+
+
+<style>
+  body{
+    margin: 0 0 0 0;
+  }
+
+  .header{
+    margin:0;
+    padding:0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    background-color: #283747;
+    color:#E5E7E9;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
+</style>
